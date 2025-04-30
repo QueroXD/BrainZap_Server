153b7919528abf36c261535c8611c883cb118e61
mbaeta8 <mbaeta8@gmail.com>
2025-04-30 16:29:20 +0200

    Clases

---
+++ BrainZap_Server/CLASSES/ClPreguntas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainZap_Server.CLASSES
+{
+    internal class ClPreguntas
+    {
+    }
+}
---
+++ BrainZap_Server/CLASSES/ClSocketServidor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainZap_Server.CLASSES
+{
+    internal class ClSocketServidor
+    {
+
+    }
+}
